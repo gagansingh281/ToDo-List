465b79ae0cc1f8bd73703914ba609dae938c945c
Gagan Singh <gagansingh281>
2020-10-22 10:37:00 +0100

    Updates

---
+++ precache-manifest.9010361adfefdb576385cee6e9fbef7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c25d504372246403cd00cab06f1f46",
+    "url": "/ToDo-List/index.html"
+  },
+  {
+    "revision": "734aa3351fda3199e40c",
+    "url": "/ToDo-List/static/css/main.5be4123e.chunk.css"
+  },
+  {
+    "revision": "280a5cc3db7e6308d0b2",
+    "url": "/ToDo-List/static/js/2.ac25479a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-List/static/js/2.ac25479a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "734aa3351fda3199e40c",
+    "url": "/ToDo-List/static/js/main.b80e591b.chunk.js"
+  },
+  {
+    "revision": "b06d0fe2d6443326c895",
+    "url": "/ToDo-List/static/js/runtime-main.6b8ffd0b.js"
+  }
+]);
